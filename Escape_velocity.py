2acaf28046e90a0f6006fbffed262fc21304cf2f
Zioles <dhrruvanand@gmail.com>
2026-04-09 18:50:54 +0530

    Update Escape_velocity.py

--- Escape_velocity.py
+++ Escape_velocity.py
@@ -7,7 +7,7 @@ def get_escape_velocity(mass, radius):
     # Formula: v = sqrt(2GM / R)
     v_esc = math.sqrt((2 * G_CONST * mass) / radius)
     return v_esc
-
+# This was a pain to code. Anyways it's developed and verified by Zioles(Dhrruv)!
 print("--- Planetary Escape Velocity Tool ---")
 # Example: Earth Mass = 5.97e24, Radius = 6.37e6
 m = float(input("Enter mass of the planet (kg): "))
